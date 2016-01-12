c30fba0275ef801dd0344742b0b7b0fbf8040cbf
GPDC <GPDC@users.noreply.github.com>
2016-01-12 00:02:19 -0800

    fixing duplicate folder

--- CryptRelay/main.cpp
+++ CryptRelay/main.cpp
@@ -1,3 +1,7 @@
+//todo: change ip checking function to a better one. iterate through the char array finding the address of all periods. take all things between that period and the last period.
+//now you have those addressess. check to see if they are numbers. if numbers, multiply each number by 10, then add them together. if > 255, return false.
+
+
 #include <iostream>
 #include <string>
 #include "ipaddress.h"
