491d4e6a0273be87e5f8a66e4ff156a26461b332
GPDC <GPDC@users.noreply.github.com>
2016-08-10 11:23:57 -0700

    Merged ProcessRecvBuf class into the ApplicationLayer class.
    This was done in order to improve OOP design. ApplicationLayer class can now be dropped into any other program and have it be able to communicate with CryptRelay.
    
    Deleted:
    - ProcessRecvBuf.h, .cpp
    - Protocol.h, .cpp
    
    Merged:
    - ProcessRecvBuf into-> ApplicationLayer

--- src/ApplicationLayer.cpp
+++ src/ApplicationLayer.cpp
@@ -10,7 +10,6 @@
 #include "ApplicationLayer.h"
 #include "SocketClass.h"
 #include "GlobalTypeHeader.h"
-#include "Protocol.h"
 #endif//__linux__
 
 #ifdef _WIN32
@@ -22,7 +21,6 @@
 #include "ApplicationLayer.h"
 #include "SocketClass.h"
 #include "GlobalTypeHeader.h"
-#include "Protocol.h"
 #endif//_WIN32
 
 
@@ -39,10 +37,27 @@
 
 #endif // __linux__
 
+#ifdef _WIN32
+#pragma warning(disable:4996)		// disable deprecated warning for fopen()
+#endif//_WIN32
+
 
 // How many characters at the beginning of the buffer that should be
 // reserved for usage of a flag, (1) and size of the message (2). (1 + 2 == 3)
 const int8_t ApplicationLayer::CR_RESERVED_BUFFER_SPACE = 3;
+
+// Flags for send() that indicate what the message is being used for.
+// CR == CryptRelay
+const int8_t ApplicationLayer::MsgFlags::NO_FLAG = 0;
+const int8_t ApplicationLayer::MsgFlags::SIZE_NOT_ASSIGNED = 0;
+const int8_t ApplicationLayer::MsgFlags::CHAT = 1;
+const int8_t ApplicationLayer::MsgFlags::ENCRYPTED_CHAT = 2;
+const int8_t ApplicationLayer::MsgFlags::FILE_NAME = 30;
+const int8_t ApplicationLayer::MsgFlags::FILE_SIZE = 31;
+const int8_t ApplicationLayer::MsgFlags::FILE_DATA = 32;
+const int8_t ApplicationLayer::MsgFlags::ENCRYPTED_FILE_NAME = 33;
+const int8_t ApplicationLayer::MsgFlags::ENCRYPTED_FILE_SIZE = 34;
+const int8_t ApplicationLayer::MsgFlags::ENCRYPTED_FILE_DATA = 35;
 
 //
 const int32_t ApplicationLayer::ARTIFICIAL_LENGTH_LIMIT_FOR_SEND = USHRT_MAX;
@@ -54,6 +69,9 @@ std::mutex ApplicationLayer::SendMutex;
 ApplicationLayer::ApplicationLayer(SocketClass* SocketClassInstance)
 {
 	Socket = SocketClassInstance;
+
+	// Specific to the ProcessRecvBuf state machine
+	memset(incoming_file_name_from_peer_cstr, 0, INCOMING_FILE_NAME_FROM_PEER_SIZE);
 }
 ApplicationLayer::~ApplicationLayer()
 {
@@ -186,7 +204,7 @@ int32_t ApplicationLayer::send(const char * sendbuf, int32_t amount_to_send)
 int64_t ApplicationLayer::sendChatStr(std::string& buf)
 {
 	int64_t message_length = buf.length();
-	if (setFlagsAndMsgSize(buf, message_length, message_length, Protocol::MsgFlags::CHAT) == -1)
+	if (setFlagsAndMsgSize(buf, message_length, message_length, ApplicationLayer::MsgFlags::CHAT) == -1)
 	{
 		DBG_DISPLAY_ERROR_LOCATION();
 		return -1;
@@ -208,7 +226,7 @@ int64_t ApplicationLayer::sendFileName(std::string name_of_file)
 	int64_t message_length = name_of_file.length();
 
 	// Put flag and message size into the first CR_RESERVED_BUFFER_SPACE chars.
-	if (setFlagsAndMsgSize(name_of_file, message_length, message_length, Protocol::MsgFlags::FILE_NAME) == -1)
+	if (setFlagsAndMsgSize(name_of_file, message_length, message_length, ApplicationLayer::MsgFlags::FILE_NAME) == -1)
 	{
 		DBG_DISPLAY_ERROR_LOCATION();
 		return -1;
@@ -236,7 +254,7 @@ int32_t ApplicationLayer::sendFileSize(char * buf, const int32_t BUF_LEN, int64_
 
 	int32_t message_length = sizeof(file_size);
 
-	if (setFlagsAndMsgSize(buf, BUF_LEN, message_length, Protocol::MsgFlags::FILE_SIZE) == -1)
+	if (setFlagsAndMsgSize(buf, BUF_LEN, message_length, ApplicationLayer::MsgFlags::FILE_SIZE) == -1)
 	{
 		DBG_DISPLAY_ERROR_LOCATION();
 		return -1;
@@ -269,7 +287,7 @@ int32_t ApplicationLayer::sendFileData(char * buf, const int32_t BUF_LEN, int32_
 		return -1;
 	}
 
-	if (setFlagsAndMsgSize(buf, BUF_LEN, message_length, Protocol::MsgFlags::FILE_DATA) == -1)
+	if (setFlagsAndMsgSize(buf, BUF_LEN, message_length, ApplicationLayer::MsgFlags::FILE_DATA) == -1)
 	{
 		DBG_DISPLAY_ERROR_LOCATION();
 		return -1;
@@ -335,7 +353,7 @@ void ApplicationLayer::loopedReceiveMessages()
 		{
 			// State machine that processes recv_buf and decides what to do
 			// based on the information in the buffer.
-			if (ProcRecv.decideActionBasedOnFlag(recv_buf, recv_buf_len, bytes) == true)
+			if (decideActionBasedOnFlag(recv_buf, recv_buf_len, bytes) == true)
 				break;
 		}
 		else if (bytes == SOCKET_ERROR)
@@ -362,17 +380,17 @@ void ApplicationLayer::loopedReceiveMessages()
 				{
 					std::cout << "Peer might have hit ctrl-c.\n";
 				}
-				if (ProcRecv.getIsFileDoneBeingWritten() == false)
+				if (is_file_done_being_written == false)
 				{
 					// Close the file that was being written inside the decideActionBasedOnFlag() state machine.
-					if (ProcRecv.WriteFile != nullptr)
+					if (WriteFile != nullptr)
 					{
-						if (fclose(ProcRecv.WriteFile) != 0)
+						if (fclose(WriteFile) != 0)
 						{
 							perror("Error closing file for writing in binary mode.\n");
 						}
 					}
-					std::cout << "File transfer was interrupted. File name: " << ProcRecv.getIncomingFileNameFromPeer() << "\n";
+					std::cout << "File transfer was interrupted. File name: " << incoming_file_name_from_peer << "\n";
 				}
 			}
 
@@ -508,4 +526,380 @@ int64_t ApplicationLayer::sendStrBuf(std::string& str_buf, int64_t message_lengt
 
 
 	return total_bytes_sent;
+}
+
+
+
+
+
+// ******************************************************************
+// *             Things related to Recv state machine               *
+// ******************************************************************
+
+// Returns false when everything is fine, and wants to be given more to process.
+bool ApplicationLayer::decideActionBasedOnFlag(char * recv_buf, int64_t recv_buf_len, int64_t received_bytes)
+{
+	position_in_recv_buf = 0;	// the current cursor position inside the buffer.
+
+								// RecvStateMachine
+	while (1)
+	{
+		switch (state)
+		{
+		case WRITE_FILE_FROM_PEER:
+		{
+			if (position_in_message < message_size)
+			{
+				int64_t amount_to_write = message_size - position_in_message;
+				if (amount_to_write > received_bytes - position_in_recv_buf)
+					amount_to_write = received_bytes - position_in_recv_buf;
+
+				bytes_written = fwrite(recv_buf + position_in_recv_buf, 1, (size_t)(amount_to_write), WriteFile);
+				if (!bytes_written)
+				{
+					perror("Error while writing the file from peer");
+
+					// close file
+					if (WriteFile != nullptr)
+					{
+						if (fclose(WriteFile))
+						{
+							perror("Error closing file for writing");
+						}
+					}
+
+					// delete file here? This could be dangerous, and should
+					// require user confirmation. Or check to make sure
+					// file didn't exist before trying to open it, then
+					// it would be safe to delete.
+
+					state = ERROR_STATE;
+					break;
+				}
+				else
+				{
+					total_bytes_written_to_file += bytes_written;
+					position_in_message += bytes_written;
+					position_in_recv_buf += bytes_written;
+
+					if (total_bytes_written_to_file >= incoming_file_size_from_peer)
+					{
+						std::cout << "Finished receiving file from peer.\n";
+						std::cout << "Expected " << incoming_file_size_from_peer << ".\n";
+						std::cout << "Wrote " << total_bytes_written_to_file << "\n";
+						std::cout << "Difference: " << incoming_file_size_from_peer - total_bytes_written_to_file << "\n";
+						state = CLOSE_FILE_FOR_WRITE;
+						is_file_done_being_written = true;
+						break;
+					}
+				}
+			}
+
+			if (position_in_recv_buf >= received_bytes)
+			{
+				return false; // go recv() again to get more bytes
+			}
+			else if (position_in_message == message_size)// must have a new message from the peer.
+			{
+				state = CHECK_FOR_FLAG;
+				break;
+			}
+
+			// this shouldn't be reached
+			std::cout << "Unrecognized message?\n";
+			std::cout << "Unreachable area, switchcase DECIDE_ACTION, recv() loop\n";
+			std::cout << "Catastrophic failure.\n";
+			state = ERROR_STATE;
+
+			break;
+		}
+		case OUTPUT_CHAT_FROM_PEER:
+		{
+			// Print out the message to terminal
+			std::cout << "\n";
+			std::cout << "Peer: ";
+			for (; (position_in_recv_buf < received_bytes) && (position_in_message < message_size);
+				++position_in_recv_buf, ++position_in_message)
+			{
+				std::cout << recv_buf[position_in_recv_buf];
+			}
+			std::cout << "\n";
+
+			if (position_in_recv_buf >= received_bytes)
+			{
+				if (position_in_message == message_size)// must have a new message from the peer.
+					state = CHECK_FOR_FLAG;
+
+				return false; // go recv() again to get more bytes
+			}
+			else if (position_in_message == message_size)// must have a new message from the peer.
+			{
+				state = CHECK_FOR_FLAG;
+				break;
+			}
+			// this shouldn't be reached
+			std::cout << "Unreachable area, switchcase DECIDE_ACTION, recv() loop\n";
+			std::cout << "Catastrophic failure.\n";
+			state = ERROR_STATE;
+			break;
+		}
+		case TAKE_FILE_NAME_FROM_PEER:
+		{
+			// Set the file name variable.
+			for (;
+				(position_in_recv_buf < received_bytes)
+				&& (position_in_message < message_size)
+				&& (position_in_message < INCOMING_FILE_NAME_FROM_PEER_SIZE - RESERVED_NULL_CHAR_FOR_FILE_NAME);
+				++position_in_recv_buf, ++position_in_message)
+			{
+				incoming_file_name_from_peer_cstr[position_in_message] = recv_buf[position_in_recv_buf];
+			}
+			// If the file name was too big, then say so, but don't error.
+			if (position_in_message >= INCOMING_FILE_NAME_FROM_PEER_SIZE - RESERVED_NULL_CHAR_FOR_FILE_NAME)
+			{
+				std::cout << "Receive File: WARNING: Peer's file name is too long. Exceeded " << INCOMING_FILE_NAME_FROM_PEER_SIZE << " characters.\n";
+				std::cout << "Receive File: File name will be incorrect on your computer.\n";
+			}
+
+			// Null terminate it.
+			if (position_in_message <= INCOMING_FILE_NAME_FROM_PEER_SIZE)
+			{
+				incoming_file_name_from_peer_cstr[position_in_message] = '\0';
+			}
+
+			// Convert it to a std::string
+			std::string temporary_incoming_file_name_from_peer(incoming_file_name_from_peer_cstr);
+			incoming_file_name_from_peer = temporary_incoming_file_name_from_peer;
+			std::cout << "# Incoming file name: " << incoming_file_name_from_peer << "\n";
+
+
+			if (position_in_message == message_size)// must have a new message from the peer.
+			{
+				state = CHECK_FOR_FLAG;
+				break;
+			}
+			// this shouldn't be reached
+			std::cout << "Unreachable area:";
+			DBG_DISPLAY_ERROR_LOCATION();
+			std::cout << "Catastrophic failure.\n";
+			state = ERROR_STATE;
+			break;
+			// Linux:
+			// Max file name length is 255 chars on most filesystems, and max path 4096 chars.
+			//
+			// Windows:
+			// Max file name length + subdirectory path is 255 chars. or MAX_PATH .. 260 chars?
+			// 1+2+256+1 or [drive][:][path][null] = 260
+			// This is not strictly true as the NTFS filesystem supports paths up to 32k characters.
+			// You can use the win32 api and "\\?\" prefix the path to use greater than 260 characters. 
+			// However using the long path "\\?\" is not a very good idea.
+		}
+		case TAKE_FILE_SIZE_FROM_PEER:
+		{
+			// convert the file size in the buffer from network int64_t to
+			// host int64_t. It assigns the variable incoming_file_size_from_peer
+			// a value.
+			if (assignFileSizeFromPeer(recv_buf, recv_buf_len, received_bytes) != FINISHED_ASSIGNING_FILE_SIZE_FROM_PEER)
+			{
+				return false;// go recv() again
+			}
+			else
+			{
+				std::cout << "Size of Peer's file: " << incoming_file_size_from_peer << "\n";
+			}
+
+			state = OPEN_FILE_FOR_WRITE;
+			break;
+		}
+		case CHECK_FOR_FLAG:
+		{
+			// Because if we are here, that means we currently
+			// aren't in a message at the moment.
+			position_in_message = 0;
+
+			if (position_in_recv_buf >= received_bytes)
+			{
+				return false;
+			}
+			else
+			{
+				message_flag = (unsigned char)recv_buf[position_in_recv_buf];
+				++position_in_recv_buf;// always have to ++ this in order to access the next element in the array.
+				state = CHECK_MESSAGE_SIZE_PART_ONE;
+				break;
+			}
+			break;
+		}
+		case CHECK_MESSAGE_SIZE_PART_ONE:
+		{
+			// Getting half of the u_short size of the message
+			if (position_in_recv_buf >= received_bytes)
+			{
+				return false;
+			}
+			else
+			{
+				message_size_part_one = (unsigned char)recv_buf[position_in_recv_buf];
+				message_size_part_one = message_size_part_one << 8;
+				++position_in_recv_buf;
+				state = CHECK_MESSAGE_SIZE_PART_TWO;
+				break;
+			}
+			break;
+		}
+		case CHECK_MESSAGE_SIZE_PART_TWO:
+		{
+			// getting the second half of the u_short size of the message
+			if (position_in_recv_buf >= received_bytes)
+			{
+				return false;
+			}
+			else
+			{
+				message_size_part_two = (unsigned char)recv_buf[position_in_recv_buf];
+				message_size = message_size_part_one | message_size_part_two;
+				++position_in_recv_buf;
+
+				// Now that we have a flag and the size of the message,
+				// set the state based on the message flag
+				if (message_flag == ApplicationLayer::MsgFlags::FILE_DATA)
+					state = WRITE_FILE_FROM_PEER;
+				else if (message_flag == ApplicationLayer::MsgFlags::FILE_NAME)
+					state = TAKE_FILE_NAME_FROM_PEER;
+				else if (message_flag == ApplicationLayer::MsgFlags::FILE_SIZE)
+					state = TAKE_FILE_SIZE_FROM_PEER;
+				else if (message_flag == ApplicationLayer::MsgFlags::CHAT)
+					state = OUTPUT_CHAT_FROM_PEER;
+				else
+				{
+					std::cout << "Fatal Error: Unidentified message has been received.\n";
+					state = ERROR_STATE;
+					break;
+				}
+
+				if (position_in_recv_buf >= received_bytes)
+				{
+					return false;
+				}
+				break;
+			}
+
+			break;
+		}
+		case OPEN_FILE_FOR_WRITE:
+		{
+			if (position_in_recv_buf >= received_bytes)
+			{
+				return false;//recv again
+			}
+			WriteFile = new FILE;
+			WriteFile = fopen(incoming_file_name_from_peer.c_str(), "wb");
+			if (WriteFile == nullptr)
+			{
+				perror("Error opening file for writing in binary mode.\n");
+			}
+
+			is_file_done_being_written = false;
+
+			// must have a new message from the peer if this is true
+			if (position_in_message == message_size)
+			{
+				state = CHECK_FOR_FLAG;
+			}
+			else // shouldn't be possible if working correctly
+			{
+				std::cout << "Catastrophic failure. RecvBuf statemachine. Unreachable area.\n";
+				state = ERROR_STATE;
+			}
+
+			break;
+		}
+		case CLOSE_FILE_FOR_WRITE:
+		{
+			if (WriteFile != nullptr)
+			{
+				if (fclose(WriteFile) != 0)		// 0 == successful close
+				{
+					perror("Error closing file for writing in binary mode");
+				}
+			}
+
+			// Set it back to default value
+			total_bytes_written_to_file = 0;
+
+
+			if (position_in_message < message_size)
+			{
+				DBG_TXT("position_in_message < message_size, closefileforwrite");
+			}
+			if (position_in_message == message_size)
+			{
+				DBG_TXT("position_in_message == message_size, everything A OK closefileforwrite");
+				std::cout << "# File transfer from peer is complete: " << incoming_file_name_from_peer << "\n";
+			}
+			if (position_in_message > message_size)
+			{
+				std::cout << "ERROR: position_in_message > message_size , closefileforwrite\n";
+			}
+			state = CHECK_FOR_FLAG;
+			break;
+		}
+		case ERROR_STATE:
+		{
+			std::cout << "Exiting with error, state machine for recv().\n";
+
+			if (WriteFile != nullptr)
+			{
+				if (fclose(WriteFile) != 0)		// 0 == successful close
+				{
+					perror("Error closing file for writing in binary mode.\n");
+					std::cout << "Error occured in RecvStateMachine, case ERROR_STATE:\n";
+				}
+			}
+
+			return true;
+		}
+		default:// currently nothing should cause this to execute.
+		{
+			std::cout << "State machine for recv() got to default. Exiting.\n";
+			return true;
+		}
+		}//end switch
+	}
+
+	return false;
+}
+
+// For use with RecvBufStateMachine only.
+int32_t ApplicationLayer::assignFileSizeFromPeer(char * recv_buf, int64_t recv_buf_len, int64_t received_bytes)
+{
+	// The peer is sending the size of his file as a int64_t.
+	// This means we will receive sizeof(int64_t) + CR_RESERVED_BUFFER_SPACE bytes.
+	// Those 8 bytes (the size of the file) will be coming to us in network byte order.
+	// Referring to each one of those 8 bytes as a 'file_size_fragment'.
+	if (file_size_fragment == 0)
+	{
+		incoming_file_size_from_peer = 0;
+	}
+
+	while (file_size_fragment < sizeof(int64_t))
+	{
+		if (position_in_recv_buf >= received_bytes)
+		{
+			return RECV_AGAIN;
+		}
+
+		// Converting it to host byte order.
+		incoming_file_size_from_peer |= (
+			(int64_t)(unsigned char)recv_buf[position_in_recv_buf])
+			<< (64 - (8 * (file_size_fragment + 1))
+				);
+		++position_in_recv_buf;
+		++position_in_message;
+		++file_size_fragment;
+	}
+
+	file_size_fragment = 0;
+
+	return FINISHED_ASSIGNING_FILE_SIZE_FROM_PEER;
 }
--- src/ApplicationLayer.h
+++ src/ApplicationLayer.h
@@ -15,14 +15,12 @@
 #include <string>
 #include <mutex> // btw, need to use std::lock_guard if you want to be able to use exceptions and avoid having it never reach the unlock.
 
-#include "ProcessRecvBuf.h"
 #endif//__linux__
 
 #ifdef _WIN32
 #include <string>
 #include <mutex> // btw, need to use std::lock_guard if you want to be able to use exceptions and avoid having it never reach the unlock.
 
-#include "ProcessRecvBuf.h"
 #endif//_WIN32
 
 // Forward declaration
@@ -56,6 +54,23 @@ public:
 	// [ flag  ][message_size][text that the user is sending to the peer..]
 	// [CR_CHAT][     38     ][Hello peer. This is my message to you!]
 	static const int8_t CR_RESERVED_BUFFER_SPACE;
+
+	// Flags that indicate what the message is being used for.
+	// enum is not used for these b/c it could break compatability when
+	// communicating with older version of this program.
+	struct MsgFlags
+	{
+		static const int8_t NO_FLAG;
+		static const int8_t SIZE_NOT_ASSIGNED;
+		static const int8_t CHAT;
+		static const int8_t ENCRYPTED_CHAT;
+		static const int8_t FILE_NAME;
+		static const int8_t FILE_SIZE;
+		static const int8_t FILE_DATA;
+		static const int8_t ENCRYPTED_FILE_DATA;
+		static const int8_t ENCRYPTED_FILE_NAME;
+		static const int8_t ENCRYPTED_FILE_SIZE;
+	};
 
 	// These functions are for doing specific actions with the send() method.
 	// They take care of all the flags, and the size of the message for you.
@@ -105,8 +120,6 @@ private:
 	ApplicationLayer(ApplicationLayer& ApplicationLayerInstance) = delete;			  // disable copy operator
 	ApplicationLayer& operator=(ApplicationLayer& ApplicationLayerInstance) = delete; // disable assignment operator
 
-	ProcessRecvBuf ProcRecv;
-
 	// This method is thread safe.
 	// Everything in CryptRelay that wants to send information to the peer
 	// MUST use this method.
@@ -124,5 +137,77 @@ private:
 	int32_t setFlagsAndMsgSize(std::string& buf, const int64_t BUF_LEN, int64_t length_of_message, int8_t flag);
 	int64_t sendStrBuf(std::string& str_buf, int64_t message_length);
 	int32_t sendCharBuf(char * buf, int32_t BUF_LEN, int32_t message_length);
+
+
+
+
+
+	// ******************************************************************
+	// *             Things related to Recv state machine               *
+	// ******************************************************************
+public:
+
+	// Here is where all messages received are processed to determine what to do with the message.
+	// Some messages might be printed to screen, others are a portion of a file transfer.
+	bool decideActionBasedOnFlag(char * recv_buf, int64_t buf_len, int64_t byte_count);
+
+	// For writing files in decideActionBasedOnFlag().
+	// It is only public because if recv() ever fails inside the ApplicationLayer class,
+	// then it needs to close the file that it was writing to.
+	FILE * WriteFile = nullptr;
+
+protected:
+private:
+	
+	enum RecvStateMachine
+	{
+		CHECK_FOR_FLAG,
+		CHECK_MESSAGE_SIZE_PART_ONE,
+		CHECK_MESSAGE_SIZE_PART_TWO,
+		WRITE_FILE_FROM_PEER,
+		TAKE_FILE_NAME_FROM_PEER,
+		TAKE_FILE_SIZE_FROM_PEER,
+		OUTPUT_CHAT_FROM_PEER,
+
+		OPEN_FILE_FOR_WRITE,
+		CLOSE_FILE_FOR_WRITE,
+
+		ERROR_STATE,
+	};
+
+	// Variables necessary for decideActionBasedOnFlag().
+	// They are here so that the state can be saved even after exiting the function.
+	int64_t position_in_recv_buf = 0;
+	int64_t state = CHECK_FOR_FLAG;
+	int64_t position_in_message = 0;	// current cursor position inside the imaginary message sent by the peer.
+	int8_t message_flag = 0;
+	int64_t message_size_part_one = 0;
+	int64_t message_size_part_two = 0;
+	int64_t message_size = 0;		    // peer told us this size
+
+										// More variables necessary for decideActionBasedOnFlag().
+										// These are related to an incoming file transfer
+	static const int64_t INCOMING_FILE_NAME_FROM_PEER_SIZE = 200;
+	static const int64_t RESERVED_NULL_CHAR_FOR_FILE_NAME = 1;
+	char incoming_file_name_from_peer_cstr[INCOMING_FILE_NAME_FROM_PEER_SIZE];
+	std::string incoming_file_name_from_peer;
+	bool is_file_done_being_written = true;
+
+
+	// Variables for decideActionBasedOnFlag()
+	// Specifically for the cases that deal with writing an incoming file from the peer.
+	int64_t bytes_read = 0;
+	int64_t bytes_written = 0;
+	int64_t total_bytes_written_to_file = 0;
+
+
+	// For use with decideActionBasedOnFlag()
+	// Using the given buffer, convert the first 8 bytes from Network to Host Long Long
+	int32_t assignFileSizeFromPeer(char * recv_buf, int64_t recv_buf_len, int64_t received_bytes);
+	// Variables necessary for assignFileSizeFromPeer();
+	int64_t file_size_fragment = 0;
+	int64_t incoming_file_size_from_peer = 0;
+	const int32_t RECV_AGAIN = 0;
+	const int32_t FINISHED_ASSIGNING_FILE_SIZE_FROM_PEER = 1;
 };
 #endif//ApplicationLayer_h__
