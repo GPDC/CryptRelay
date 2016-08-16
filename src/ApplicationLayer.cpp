1251b00be4faefd64fffada718d4e5d306805068
GPDC <GPDC@users.noreply.github.com>
2016-08-16 16:02:06 -0700

    Improved comments, and intoBufferHostToNetworkLongLong() now properly checks for BUF_LEN.
    Now if someone changes the CR_RESERVED_BUFFER_SPACE variable, intoBufferHostToNetworkLongLong() will not break.

--- src/ApplicationLayer.cpp
+++ src/ApplicationLayer.cpp
@@ -111,19 +111,8 @@ ApplicationLayer::~ApplicationLayer()
 #endif//_WIN32
 }
 
-// buf: This is where the flags and size of the message will be set.
-// It will use the first CR_RESERVED_BUFFER_SPACE bytes of the buffer
-// for the flags. That means it is up to whoever is giving the char * buf
-// to this method to not use the first CR_RESERVED_BUFFER_SPACE bytes
-// or else they will be overwritten by this method.
-// BUF_LEN: length of the buf.
-// length_of_msg: This is the amount from the buf that you want to send.
-// flag: The type of message that you are sending. Flags are located
-// in the structure ApplicationLayer::MsgFlags
-// Example operation:
-// bytes_read = fread(buf + CR_RESERVED_BUFFER_SPACE, 1, BUF_LEN - CR_RESERVED_BUFFER_SPACE, ReadFile);
-// setFlagsAndMsgSize(buf, BUF_LEN, bytes_read, ApplicationLayer::MsgFlags::CR_FILE);
-// Returns 0 on success, -1 on error
+// Returns 0, success
+// Returns -1, error
 int32_t ApplicationLayer::setFlagsAndMsgSize(char * buf, const int32_t BUF_LEN, int32_t length_of_msg, int8_t flag)
 {
 	if (buf == nullptr)
@@ -163,7 +152,8 @@ int32_t ApplicationLayer::setFlagsAndMsgSize(char * buf, const int32_t BUF_LEN, 
 	}
 }
 
-// Returns 0 on success, -1 on error
+// Returns 0, success
+// Returns -1, error
 int32_t ApplicationLayer::setFlagsAndMsgSize(std::string& buf, const int64_t BUF_LEN, int64_t length_of_msg, int8_t flag)
 {
 	const int64_t MAX_STRING_LENGTH = buf.max_size();
@@ -192,14 +182,6 @@ int32_t ApplicationLayer::setFlagsAndMsgSize(std::string& buf, const int64_t BUF
 }
 
 
-// All information that gets sent over the network MUST go through
-// this method. This is to avoid abnormal behavior / problems
-// with multiple threads trying to send() using the same socket.
-// The flag argument is to tell the receiver of these messages
-// how to interpret the incoming message. For example as a file,
-// or as a chat message.
-// To see a list of flags, look in the header file.
-// Returns SOCKET_ERROR on error, and total amount of bytes sent on success.
 int32_t ApplicationLayer::send(const char * sendbuf, int32_t amount_to_send)
 {
 	// Whatever thread gets here first, will lock
@@ -208,7 +190,7 @@ int32_t ApplicationLayer::send(const char * sendbuf, int32_t amount_to_send)
 	// and won't be able to go past this point until the
 	// thread that got here first unlocks it.
 	SendMutex.lock();
-	total_amount_sent = amount_to_send;
+	total_bytes_sent = amount_to_send;
 	
 	do
 	{
@@ -227,15 +209,16 @@ int32_t ApplicationLayer::send(const char * sendbuf, int32_t amount_to_send)
 
 	} while (amount_to_send > 0);
 
-	total_amount_sent -= amount_to_send;
+	total_bytes_sent -= amount_to_send;
 
 	// Unlock the door now that the thread is done with this function.
 	SendMutex.unlock();
 
-	return total_amount_sent;// returning the amount of bytes sent.
+	return total_bytes_sent;// returning the amount of bytes sent.
 }
 
-// returns total bytes sent, succes
+// This method writes over the first CR_RESERVED_BUFFER_SPACE bytes of the given buffer.
+// returns total bytes sent, success
 // returns -1, error.
 int64_t ApplicationLayer::sendChatStr(std::string& buf)
 {
@@ -256,8 +239,9 @@ int64_t ApplicationLayer::sendChatStr(std::string& buf)
 	return total_bytes_sent;
 }
 
-// returns total bytes sent, succes
-// returns -1, error
+// This method writes over the first CR_RESERVED_BUFFER_SPACE bytes of the given buffer.
+// returns total bytes sent, success
+// returns -1, error.
 int64_t ApplicationLayer::sendFileName(std::string name_of_file)
 {
 	int64_t message_length = name_of_file.length();
@@ -335,8 +319,9 @@ int32_t ApplicationLayer::sendFileData(char * buf, const int32_t BUF_LEN, int32_
 	return total_bytes_sent;
 }
 
-// returns -1, error
-// returns 0, success
+// This method writes over the first CR_RESERVED_BUFFER_SPACE bytes of the given buffer.
+// returns total bytes sent, success
+// returns -1, error.
 int32_t ApplicationLayer::sendFileTransferComplete(char * buf, const int32_t BUF_LEN)
 {
 	if (buf == nullptr)
@@ -366,16 +351,18 @@ int32_t ApplicationLayer::sendFileTransferComplete(char * buf, const int32_t BUF
 // Using the given variable, it will convert it to network long long and place it into the given buffer.
 int32_t ApplicationLayer::intoBufferHostToNetworkLongLong(char * buf, const int64_t BUF_LEN, int64_t variable_to_convert)
 {
-	if (BUF_LEN > (int64_t)sizeof(variable_to_convert))
+	if (BUF_LEN > (int64_t)sizeof(variable_to_convert) + CR_RESERVED_BUFFER_SPACE)
 	{
-		buf[3] = (char)(variable_to_convert >> 56);
-		buf[4] = (char)(variable_to_convert >> 48);
-		buf[5] = (char)(variable_to_convert >> 40);
-		buf[6] = (char)(variable_to_convert >> 32);
-		buf[7] = (char)(variable_to_convert >> 24);
-		buf[8] = (char)(variable_to_convert >> 16);
-		buf[9] = (char)(variable_to_convert >> 8);
-		buf[10] = (char)variable_to_convert;
+		// Since the first CR_RESERVED_BUFFER_SPACE bytes shouldn't be touched,
+		// put the variable_to_convert into the buffer starting at CR_RESERVED_BUFFER_SPACE.
+		buf[CR_RESERVED_BUFFER_SPACE] = (char)(variable_to_convert >> 56);
+		buf[CR_RESERVED_BUFFER_SPACE + 1] = (char)(variable_to_convert >> 48);
+		buf[CR_RESERVED_BUFFER_SPACE + 2] = (char)(variable_to_convert >> 40);
+		buf[CR_RESERVED_BUFFER_SPACE + 3] = (char)(variable_to_convert >> 32);
+		buf[CR_RESERVED_BUFFER_SPACE + 4] = (char)(variable_to_convert >> 24);
+		buf[CR_RESERVED_BUFFER_SPACE + 5] = (char)(variable_to_convert >> 16);
+		buf[CR_RESERVED_BUFFER_SPACE + 6] = (char)(variable_to_convert >> 8);
+		buf[CR_RESERVED_BUFFER_SPACE + 7] = (char)variable_to_convert;
 		return 0;
 	}
 
@@ -383,24 +370,20 @@ int32_t ApplicationLayer::intoBufferHostToNetworkLongLong(char * buf, const int6
 	return -1;
 }
 
-
-// remote_host is /* optional */    default == "Peer".
-// remote_host is the IP that we are connected to.
-// To find out who we were connected to, use getnameinfo()
+// Helpful Information:
+// Any time a 'message' is mentioned, it is referring to the idea of
+// a whole message. That message may be broken up into packets and sent over the
+// network, and when it gets to this recv() loop, it will read in as much as it can
+// at a time. The entirety of a message is not always contained in the recv_buf,
+// because the message could be so large that you will have to recv() multiple times
+// in order to get the whole message. 
+// References to 'message' have nothing to do with the recv_buf.
+// the size and type of a message is determined by the peer, and the peer tells us
+// the type and size of the message in the first CR_RESERVED_BUFFER_SPACE characters
+// of his message that he sent us. The CR_RESERVED_BUFFER_SPACE characters do not
+// count as part of the message.
 void ApplicationLayer::loopedReceiveMessages()
 {
-	// Helpful Information:
-	// Any time a 'message' is mentioned, it is referring to the idea of
-	// a whole message. That message may be broken up into packets and sent over the
-	// network, and when it gets to this recv() loop, it will read in as much as it can
-	// at a time. The entirety of a message is not always contained in the recv_buf,
-	// because the message could be so large that you will have to recv() multiple times
-	// in order to get the whole message. 
-	// References to 'message' have nothing to do with the recv_buf.
-	// the size and type of a message is determined by the peer, and the peer tells us
-	// the type and size of the message in the first CR_RESERVED_BUFFER_SPACE characters
-	// of his message that he sent us.
-
 	// Receive until the peer shuts down the connection
 	if (verbose_output == true)
 		std::cout << "Recv loop started...\n";
@@ -477,6 +460,7 @@ void ApplicationLayer::loopedReceiveMessages()
 
 	return;
 }
+
 // returns -1, error
 // returns 0, success
 int32_t ApplicationLayer::endConnection()
@@ -607,10 +591,12 @@ int64_t ApplicationLayer::sendStrBuf(std::string& str_buf, int64_t message_lengt
 // *             Things related to Recv state machine               *
 // ******************************************************************
 
-// Returns false when everything is fine, and wants to be given more to process.
+// Returns 0, succes, and wants to recv() more bytes.
+// Returns -1, error
 int32_t ApplicationLayer::decideActionBasedOnFlag(char * recv_buf, int64_t recv_buf_len, int64_t received_bytes)
 {
-	position_in_recv_buf = 0;	// the current cursor position inside the buffer.
+	// the current cursor position inside the buffer.
+	position_in_recv_buf = 0;	
 
 	// RecvStateMachine
 	while (1)
--- src/ApplicationLayer.h
+++ src/ApplicationLayer.h
@@ -5,11 +5,11 @@
 // The ApplicationLayer is the last step in handling all outbound data before it
 // reaches the network and the first step in handling all inbound data after it
 // comes from the network. Class ApplicationLayer has all things necessary to send a
-// message with the necessary flags, and size of the message included in the message.
-// Inside the ApplicationLayer class is another class called ProcessRecvBuf.
-// The ProcessRecvBuf class is responsible for interpreting incoming messages
-// based on the flag, and size of the message. It will decide what to do
-// with the data that it was sent based on the flag.
+// message with the necessary flags, and size of the message.
+// Inside the ApplicationLayer class is the ProcessRecvBuf state machine. The function that
+// contains the switch case for it is decideActionBasedOnFlag(). It is responsible for
+// interpreting incoming messages based on the flag, and size of the message. It will
+// decide what to do with the data that it was sent based on the flag.
 
 #ifdef __linux__
 #include <sys/types.h>
@@ -36,8 +36,9 @@ class ApplicationLayer
 	// Typedef section
 public:
 	// Typedefs for callbacks
-	typedef void callback_fn_set_exit_now(bool value);
-	typedef bool& callback_fn_get_exit_now();
+	typedef void callback_fn_set_exit_now(bool value); // for setting the bool variable if you want the program to exit.
+	typedef bool& callback_fn_get_exit_now(); // for viewing the bool variable to see if the program wants to exit.
+
 
 #ifdef __linux__
 	typedef int32_t SOCKET;
@@ -95,38 +96,9 @@ public:
 		static const int8_t FILE_TRANSFER_COMPLETE;
 	};
 
-	// These functions are for doing specific actions with the send() method.
-	// They take care of all the flags, and the size of the message for you.
-	// For example, sendChat(buf, BUF_LEN, message_length); would
-	// assign the MsgFlags::CHAT flag in buf[0], and the message_length
-	// would be assigned for you in buf[1] and buf[2].
-
-	// Anyone who calls this function must make sure they put information into their
-	// buffer starting at array index CR_RESERVED_BUFFER_SPACE or else those bytes
-	// will be overwritten.
-	//int64_t sendChat(char * buf, const int32_t BUF_LEN, int32_t message_length);
-	int64_t sendChatStr(std::string& buf);
-	int64_t sendFileName(std::string name_of_file);
-	int32_t sendFileSize(char * buf, const int32_t BUF_LEN, int64_t file_size);
-	int32_t sendFileData(char * buf, const int32_t BUF_LEN, int32_t message_length);
-	int32_t sendFileTransferComplete(char * buf, const int32_t BUF_LEN);
-
-	// SHOULD THIS BE PUBLIC OR PRIVATE?
-	// Used for copying a host byte order LongLong into the given buffer as a network byte order.
-	int32_t intoBufferHostToNetworkLongLong(char * buf, const int64_t BUF_LEN, int64_t variable_to_convert);
-
-	// This will shutdown() the connection on the socket and close() it.
-	// This will interrupt recv() if it is currently blocking.
-	int32_t endConnection();
-
-	// ApplicationLayer class will do the receiving and then send the information to ProcessRecvBuf class
-	// Thread used to handle receiving messages.
-	void loopedReceiveMessages();	
-
-
 	// This artifical limit is to make it so that the send() method does not
 	// prevent anything else from being sent for an excessively long period
-	// of time. 
+	// of time.
 	// For example if you try sending a 50gb file, send() would
 	// normally just be taken up by that file transfer for a very long time,
 	// because of the mutex, and thus not allowing any chat message to go
@@ -134,7 +106,50 @@ public:
 	// a chance to access send() method during the file transfer.
 	static const int32_t ARTIFICIAL_LENGTH_LIMIT_FOR_SEND;
 
-	// ----------------------------------------------------------------------
+	//						END IMPORTANT INFORMATION
+	// =========================================================================
+
+
+
+	// ------------------------------------------------------------------------------------
+	// These functions are for doing specific actions with the send() method.
+	// They take care of all the flags, and the size of the message for you.
+	// For example, sendChat(buf, BUF_LEN, message_length); would
+	// assign the MsgFlags::CHAT flag in buf[0], and the message_length
+	// would be assigned for you in buf[1] and buf[2].
+
+	// Anyone who calls the functions that take a char * buf arg must make sure they
+	// put information into their buffer starting at array index CR_RESERVED_BUFFER_SPACE
+	// or else those bytes will be overwritten. This does not apply to std::string bufs.
+
+	// They all return in this fashion:
+	// returns total bytes sent, success
+	// returns -1, error.
+
+	int64_t sendChatStr(std::string& buf);
+	int64_t sendFileName(std::string name_of_file);
+
+	// Writes over the first CR_RESERVED_BUFFER_SPACE bytes of the given buffer.
+	// It then writes the size of the file into the next sizeof(file_size) bytes of the given buffer.
+	int32_t sendFileSize(char * buf, const int32_t BUF_LEN, int64_t file_size);
+
+	int32_t sendFileData(char * buf, const int32_t BUF_LEN, int32_t message_length);
+
+	// Tell the peer that you have finished sending all the file data to them.
+	int32_t sendFileTransferComplete(char * buf, const int32_t BUF_LEN);
+	// ====================================================================================
+
+
+
+	// Call this if you want the ApplicationLayer to stop the connection with the peer.
+	// This will shutdown() the connection on the socket and close() it.
+	// This will interrupt recv() if it is currently blocking.
+	// returns -1, error
+	// returns 0, success
+	int32_t endConnection();
+
+	// recv()s data from peer and gives it to decideActionBasedOnFlag() to process the data.
+	void loopedReceiveMessages();
 
 
 protected:
@@ -143,27 +158,54 @@ private:
 	// Prevent anyone from copying this class
 	ApplicationLayer(ApplicationLayer& ApplicationLayerInstance) = delete;			  // disable copy operator
 	ApplicationLayer& operator=(ApplicationLayer& ApplicationLayerInstance) = delete; // disable assignment operator
+	
+	IXBerkeleySockets * Socket;
+
+	// ApplicationLayer expects this socket to have an active connection with the peer.
+	SOCKET fd_socket;
 
 	// This method is thread safe.
-	// Everything in CryptRelay that wants to send information to the peer
-	// MUST use this method.
+	// All information that gets sent over the network MUST go through
+	// this method. This is to avoid abnormal behavior / problems
+	// with multiple threads trying to send() using the same socket.
+	// Returns SOCKET_ERROR, error (see ApplicationLayer.cpp for linux #define)
+	// Returns total amount of bytes sent, success.
 	int32_t send(const char * sendbuf, int32_t amount_to_send);
 	// These are for the send() method
-	int32_t total_amount_sent = 0;
+	int32_t total_bytes_sent = 0;
 	int32_t bytes_sent = 0;
 	static std::mutex SendMutex;
 
-	IXBerkeleySockets * Socket;
-
-	SOCKET fd_socket;
-
-
+	
 	// It puts the flag and size of the message into the given buffer.
+	// buf: This is where the flags and size of the message will be set.
+	// It will use the first CR_RESERVED_BUFFER_SPACE bytes of the buffer
+	// for the flags. That means it is up to whoever is giving the char * buf
+	// to this method to not use the first CR_RESERVED_BUFFER_SPACE bytes
+	// or else they will be overwritten by this method.
+	// BUF_LEN: length of the buf.
+	// length_of_msg: This is the amount from the buf that you want to send.
+	// flag: The type of message that you are sending. Flags are located
+	// in the structure ApplicationLayer::MsgFlags
+	// Example operation:
+	// bytes_read = fread(buf + CR_RESERVED_BUFFER_SPACE, 1, BUF_LEN - CR_RESERVED_BUFFER_SPACE, ReadFile);
+	// setFlagsAndMsgSize(buf, BUF_LEN, bytes_read, ApplicationLayer::MsgFlags::CR_FILE);
+	// Returns 0, success
+	// Returns -1, error
 	int32_t setFlagsAndMsgSize(char * buf, const int32_t BUF_LEN, int32_t length_of_message, int8_t flag);
 	int32_t setFlagsAndMsgSize(std::string& buf, const int64_t BUF_LEN, int64_t length_of_message, int8_t flag);
 
+	// returns total bytes sent, success
+	// returns -1, error
 	int64_t sendStrBuf(std::string& str_buf, int64_t message_length);
+
+	// returns total bytes sent, success
+	// returns -1, error
 	int32_t sendCharBuf(char * buf, int32_t BUF_LEN, int32_t message_length);
+
+	// Used for copying a host byte order LongLong into the given buffer as a network byte order.
+	// Using the given variable, it will convert it to network long long and place it into the given buffer.
+	int32_t intoBufferHostToNetworkLongLong(char * buf, const int64_t BUF_LEN, int64_t variable_to_convert);
 
 	// Cross-platform WSAStartup();
 	// For every WSAStartup() that is called, a WSACleanup() must be called.
@@ -175,8 +217,8 @@ private:
 
 
 private:
-	callback_fn_set_exit_now * callbackSetExitNow = nullptr; // for setting the bool exit_now variable.
-	callback_fn_get_exit_now * callbackGetExitNow = nullptr; // for viewing the bool exit_now variable.
+	callback_fn_set_exit_now * callbackSetExitNow = nullptr; // for setting the bool variable if you want the program to exit.
+	callback_fn_get_exit_now * callbackGetExitNow = nullptr; // for viewing the bool variable to see if the program wants to exit.
 
 
 
@@ -221,21 +263,22 @@ private:
 
 	// Variables necessary for decideActionBasedOnFlag().
 	// They are here so that the state can be saved even after exiting the function.
-	int64_t position_in_recv_buf = 0;
+	int64_t position_in_recv_buf = 0;   // the current cursor position inside the buffer.
 	int64_t state = CHECK_FOR_FLAG;
 	int64_t position_in_message = 0;	// current cursor position inside the imaginary message sent by the peer.
-	int8_t message_flag = 0;
+	int8_t message_flag = 0;			// The flag that the peer sent us. (See struct MsgFlags for a list of flags)
 	int64_t message_size_part_one = 0;
 	int64_t message_size_part_two = 0;
-	int64_t message_size = 0;		    // peer told us this size
+	int64_t message_size = 0;		    // peer told us the size of his message.
 
-										// More variables necessary for decideActionBasedOnFlag().
-										// These are related to an incoming file transfer
-	static const int64_t INCOMING_FILE_NAME_FROM_PEER_SIZE = 200;
+	// Variables necessary for decideActionBasedOnFlag().
+	// These are related to an incoming file transfer
 	static const int64_t RESERVED_NULL_CHAR_FOR_FILE_NAME = 1;
-	char incoming_file_name_from_peer_cstr[INCOMING_FILE_NAME_FROM_PEER_SIZE];
-	std::string incoming_file_name_from_peer;
-	bool is_file_done_being_written = true;
+	static const int64_t INCOMING_FILE_NAME_FROM_PEER_SIZE = 200; // size of the cstring char array.
+	char incoming_file_name_from_peer_cstr[INCOMING_FILE_NAME_FROM_PEER_SIZE]; // The peer told us his file name, and that file name is initially placed in here.
+	std::string incoming_file_name_from_peer; // After the file name was initially placed in the incoming_file_name_from_peer_cstr, it is placed here for ease of use.
+	// This is to let the program know that if an error occured while a file was still being written, then it should close the file.
+	bool is_file_done_being_written = true; 
 
 
 	// Variables for decideActionBasedOnFlag()
@@ -246,12 +289,19 @@ private:
 
 
 	// For use with decideActionBasedOnFlag()
-	// Using the given buffer, convert the first 8 bytes from Network to Host Long Long
+	// Using the given buffer, convert the first 8 bytes from Network to Host Long Long.
+	// It then assigns the variable incmoing_file_size_from_peer that host byte order value.
+	// Returns RECV_AGAIN if it needs more bytes from the peer.
+	// Returns FINISHED_ASSIGNING_FILE_SIZE_FROM_PEER if it has completed its task.
 	int32_t assignFileSizeFromPeer(char * recv_buf, int64_t recv_buf_len, int64_t received_bytes);
 	// Variables necessary for assignFileSizeFromPeer();
 	int64_t file_size_fragment = 0;
 	int64_t incoming_file_size_from_peer = 0;
 	const int32_t RECV_AGAIN = 0;
 	const int32_t FINISHED_ASSIGNING_FILE_SIZE_FROM_PEER = 1;
+
+	// ******************************************************************
+	// *         End of things related to Recv state machine            *
+	// ******************************************************************
 };
 #endif//ApplicationLayer_h__
